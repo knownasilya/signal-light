93471826bd19591eda02743243e3303e6b6e78b8
Ilya Radchenko <knownasilya@gmail.com>
2019-01-28 19:36:48 -0500

    fix: prod build and add manifest

---
+++ config/manifest.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/zonkyio/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "Signal Light",
+    short_name: "Signal Light",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
